c0f8ea6fc946bf60dec0d477e56ee88c5ee0d88a
johnst101 <107722987+johnst101@users.noreply.github.com>
2025-11-30 08:39:51 -0600

    Update user table with first and last name

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -13,6 +13,8 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
     email = Column(String(255), unique=True, index=True, nullable=False)
     password_hash = Column(String(255), nullable=False)
     username = Column(String(50), unique=True, index=True, nullable=True)
