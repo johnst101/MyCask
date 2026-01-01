27e0aebbd756a563bbc4f13482e2641747948653
Tyler Johnson <tylercjohnson16@gmail.com>
2026-01-01 17:23:20 -0600

    Update Profile page with more details. More to complete

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,10 +1,57 @@
+import { useParams } from 'react-router-dom';
+import SubmitButton from '../components/SubmitButton';
+import logo from '../assets/mc_logo-removebg.png';
+
 const Profile = () => {
+  const { firstName } = useParams<{ firstName: string }>(); // FIXME: is this necessary? Can I just get the username from the JWT token?
+  // TODO: get user profile data from backend based on who is logged in (from JWT token? How else should I get this information?)
+
   return (
-    <div className="@container bg-white rounded-md w-full max-w-md md:max-w-lg lg:max-w-xl mx-auto my-4 sm:my-6 md:my-8 px-4 py-4 sm:px-6 sm:py-6 md:px-8 md:py-8 max-h-[calc(100vh-2rem)] sm:max-h-[calc(100vh-3rem)] md:max-h-[calc(100vh-4rem)] overflow-y-auto">
-      <div className="flex flex-col items-center justify-center py-8">
-        <h1 className="font-playfair-display font-bold text-xl @[400px]:text-2xl @[600px]:text-3xl text-center text-deep-green mb-4 sm:mb-6">
-          Profile
+    <div className="card-container animate-fade-in">
+      <div className="flex justify-between items-center">
+        <h1 className="heading-profile">
+          <span className="font-playfair-display font-light text-text-secondary text-xl @[400px]:text-2xl @[600px]:text-3xl">
+            Hello,
+          </span>{' '}
+          <br />
+          {firstName || 'User'}
         </h1>
+        <img src={logo} alt="MyCask Logo" className="profile-picture" />
+      </div>
+      <form className="form-container">
+        <input
+          className="form-input"
+          type="email"
+          placeholder="Current Email"
+        />
+        <input
+          className="form-input"
+          type="text"
+          placeholder="Current First Name"
+        />
+        <input
+          className="form-input"
+          type="text"
+          placeholder="Current Last Name"
+        />
+        <input
+          className="form-input"
+          type="text"
+          placeholder="Current Username"
+        />
+        <button className="password-reset-button">
+          Update Password (add redirect to password reset page)
+        </button>
+        <SubmitButton
+          isLoading={false}
+          loadingText="Updating profile..."
+          defaultText="Update Profile"
+        />
+      </form>
+      <div className="flex justify-center items-center mt-4">
+        <a href="/delete-account" className="text-red-500">
+          Delete Account
+        </a>
       </div>
     </div>
   );
